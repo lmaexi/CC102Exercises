9a3fada98258a25c4fd557254451358df4c0f5bc
eramaee <ty202500154@wmsu.edu.ph>
2026-02-20 08:41:17 +0800

    Add files via upload

---
+++ AncajasCC102Lab4_2.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    const int salesPerson = 4, product = 5;
+    double amount = 0;
+    int person, products;
+    char slip, choice;
+    double prodTotal = 0;
+    double overallTotal = 0;
+
+    double sales [product][salesPerson] = {0};
+
+    do{
+        double amount = 0;
+        double prodTotal = 0;
+        double overallTotal = 0;
+
+        double sales [product][salesPerson] = {0};
+
+        system("cls");
+        cout<<"\tMonthly Sales Summary\n"<<endl;
+
+    do{
+        cout<<"Salesperson{1-4}: ";
+        cin>>person;
+        cout<<"Product{1-5}: ";
+        cin>>products;
+        cout<<"Amount: ";
+        cin>>amount;
+
+        if (person >=1 && person <=4 && products >= 1 && products <=5){
+            sales[products -1][person-1] += amount;
+        } else {
+            cout<<"Invalid Slip, Do not Exceed the Limit!"<<endl;
+        }
+
+        cout<<"Add Another Slip: ";
+        cin>>slip;
+        cout<<"\n";
+    }while(slip == 'y' || slip == 'Y');
+
+    //this is the title and header of the final table
+    cout<<"\n\n";
+    cout<<"\t\tSalesperson"<<endl;
+    cout<<"Product\t";
+    for (int i=0; i<salesPerson; i++){
+        cout<<i+1<<"\t";
+    }
+    cout<<"TOTAL"<<endl;
+
+    for (int j=0; j<product; j++){
+        prodTotal =0;
+        cout<<j+1<<"\t";
+        for (int k=0; k<salesPerson; k++){
+            cout<<sales[j][k]<<"\t";
+            prodTotal += sales[j][k];
+        }
+        cout<<prodTotal<<endl;
+        overallTotal += prodTotal;
+    }
+
+    cout<<"Total\t";
+    for (int l=0; l<salesPerson; l++){
+        double lasTotal = 0;
+        for (int m=0; m<product; m++){
+            lasTotal += sales[m][l];
+        }
+        cout<<lasTotal<<"\t";
+    }
+    cout<<overallTotal<<endl;
+
+    cout<<"\nDo You Want to Try Again[Y/N]: ";
+    cin>>choice;
+    }while(choice == 'y' || choice == 'Y');
+
+
+    return 0;
+}
