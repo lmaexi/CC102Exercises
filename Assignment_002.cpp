f9c010de6ceeb9766e63f9ac5378b27bff1188c1
eramaee <ty202500154@wmsu.edu.ph>
2026-03-16 20:37:36 +0800

    Update Assignment_002.cpp

--- Assignment_002.cpp
+++ Assignment_002.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 void computeGrade(int *matrix, int student, int subj);
 
 int main(){
     int students, subjects;
+    char choice;
 
-    cout<<"Enter Number of Student: ";
-    cin>>students;
-
-    cout<<"Enter Number of Subjects: ";
-    cin>>subjects;
-
-    int grades[students][subjects];
-
-    cout<<"\nENTER STUDENTS GRADE \n"<<endl;
-    for(int i=0; i<students; i++){
-        cout<<"Student "<<i+1<<endl;
-
-        for (int j=0; j<subjects; j++){
-            cout<<"Subject "<<j+1<<": ";
-            cin>>grades[i][j];
+    do{
+        system("cls");
+        cout<<"Enter Number of Student: ";
+        cin>>students;
+    
+        cout<<"Enter Number of Subjects: ";
+        cin>>subjects;
+    
+        int grades[students][subjects];
+    
+        cout<<"\nENTER STUDENTS GRADE \n"<<endl;
+        for(int i=0; i<students; i++){
+            cout<<"Student "<<i+1<<endl;
+    
+            for (int j=0; j<subjects; j++){
+                cout<<"Subject "<<j+1<<": ";
+                cin>>grades[i][j];
+            }
+            cout<<endl;
         }
-        cout<<endl;
-    }
+    
+        computeGrade(&grades[0][0], students, subjects);
 
-    computeGrade(&grades[0][0], students, subjects);
+        cout<<"\nDo you want to Input Again?[y/n]: ";
+        cin>>choice;
+
+    } while (choice == 'y' || choice == 'Y');
 
     return 0;
 }
@@ -33,6 +42,7 @@ int main(){
 
 void computeGrade(int *matrix, int student, int subj){
     int highest = *(matrix);
+    int avrg = 0;
 
     cout<<"\n\tSTUDENT GRADE MATRIX\n";
 
@@ -49,7 +59,7 @@ void computeGrade(int *matrix, int student, int subj){
                 highest = grade;
             }
         }
-        int avrg=sum/subj;
+        avrg=sum/subj;
 
         cout<<" | Student "<<i+1<<" Average : "<<avrg;
         cout<<endl;
